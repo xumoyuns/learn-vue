0bd1c4b590307afac5dae410fcd35a42849f0c73
xumoyuns <xumoyunwebcoder@gmail.com>
2023-05-04 12:08:50 +0500

    test app create

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div class="">
+        <form action="">
+            
+        </form>
+
+
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
